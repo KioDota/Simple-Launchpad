46367be4e1619d8dae48973ba3c271f8a54d595e
MarvinUnger <marvinunger98@gmail.com>
2019-06-14 11:19:10 +0200

    overlaying sounds works now

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,3 @@
-var Snare1 = new Audio();
-Snare1.src = 'https://instaud.io/_/3Oal.wav';
-var HiHat1 = new Audio();
-HiHat1.src = 'https://instaud.io/_/3OaS.wav';
-
-
 document.addEventListener('keydown', function (evt) {
     console.log(evt.keyCode);
     switch (evt.keyCode) {
@@ -11,7 +5,7 @@ document.addEventListener('keydown', function (evt) {
             var Kick1 = new Audio();
             Kick1.src = 'https://instaud.io/_/3Oaj.wav';
             if (typeof Kick1.loop == 'boolean') {
-                Kick1.loop = true;
+                Kick1.loop = false;
             } else {
                 Kick1.addEventListener('ended', function () {
                     this.currentTime = 0;
@@ -21,16 +15,31 @@ document.addEventListener('keydown', function (evt) {
             Kick1.play();
             break;
 
-        case 83:  //keyCode for S
-            Kick1.pause();
-            break;
-
         case 68: //keyCode for D
-            Snare1.play();
-            break;
+        var Snare1 = new Audio();
+        Snare1.src = 'https://instaud.io/_/3Oal.wav';
+        if (typeof Snare1.loop == 'boolean') {
+            Snare1.loop = false;
+        } else {
+            Snare1.addEventListener('ended', function () {
+                this.currentTime = 0;
+                this.play();
+            }, false);
+        }
+        Snare1.play();
+        break;
 
         case 79: //keyCode for O
-            HiHat1.play();
-            break;
-    }
+        var HiHat1 = new Audio();
+        HiHat1.src = 'https://instaud.io/_/3OaS.wav';
+        if (typeof HiHat1.loop == 'boolean') {
+            HiHat1.loop = false;
+        } else {
+            HiHat1.addEventListener('ended', function () {
+                this.currentTime = 0;
+                this.play();
+            }, false);
+        }
+        HiHat1.play();
+        break;}
 });
